54baa4f770c047039ddef9883279f736bc14395a
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-11-24 15:11:29 +0200

    Updating List.js
    Udpating list.js by including placeholders

--- soft/src/list.js
+++ soft/src/list.js
@@ -1,6 +1,12 @@
 import ListItem from './ListItem'
+import { useState } from 'react'
 
 export default function List(){
+    [setCustomer, setNewCustomer]=useState('customer')
+
+
+    fetch('./')
+    .then 
 
 return(
 < ListItem />
